19d7c9d9432c496289779b880d624468e26a412f
marduzca <arduz.miguel@gmail.com>
2020-06-04 18:35:40 +0200

    Update Readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mimica Backend
+Website to play charades remotely
+
+---
+
+## How to:
+
+### Execute
+```sh
+> npm start
+```
+
+### Run tests 
+```sh
+> npm test
+```
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).json({
-    message: 'Welcome to Mimica Backend!'
+    message: 'Welcome to Mimica Backend v2.0!'
   });
 });
 
