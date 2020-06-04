5b7a139c94fae504c03eba92954d5fbb38be71db
marduzca <arduz.miguel@gmail.com>
2020-06-04 18:22:42 +0200

    Small change

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).json({
-    message: 'Welcome to Mimica!'
+    message: 'Welcome to Mimica Backend!'
   });
 });
 
