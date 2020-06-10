8e0c0c1cd0c4bd331a805ced610a5b3f39bc0c93
marduzca <arduz.miguel@gmail.com>
2020-06-10 16:08:09 +0200

    Remove logs

--- routes/videoStream.js
+++ routes/videoStream.js
@@ -4,46 +4,26 @@ const router = express.Router();
 const users = {};
 
 module.exports = function (io) {
-    try {
-        io.on('connection', socket => {
-            if (!users[socket.id]) {
-                console.log('ON CONNECTION ' + socket.id);
-                users[socket.id] = socket.id;
-            }
-            console.log('BREAKPOINT 1, USERS: ' + JSON.stringify(users));
+    io.on('connection', socket => {
+        if (!users[socket.id]) {
+            users[socket.id] = socket.id;
+        }
 
-            socket.emit("yourID", socket.id);
-            io.sockets.emit("allUsers", users);
-            console.log('BREAKPOINT 2, USERS: ' + JSON.stringify(users));
+        socket.emit("yourID", socket.id);
+        io.sockets.emit("allUsers", users);
 
-            socket.on('disconnect', () => {
-                console.log('ON disconnect ');
-                delete users[socket.id];
-            })
+        socket.on('disconnect', () => {
+            delete users[socket.id];
+        })
 
-            console.log('BREAKPOINT 3, USERS: ' + JSON.stringify(users));
+        socket.on("callUser", (data) => {
+            io.to(data.userToCall).emit('hey', { signal: data.signalData, from: data.from });
+        })
 
-
-            socket.on("callUser", (data) => {
-                console.log('ON callUSER ' + data);
-                io.to(data.userToCall).emit('hey', { signal: data.signalData, from: data.from });
-            })
-
-            console.log('BREAKPOINT 4, USERS: ' + JSON.stringify(users));
-
-
-            socket.on("acceptCall", (data) => {
-                console.log('ON acceptCall ' + data);
-                io.to(data.to).emit('callAccepted', data.signal);
-            })
-
-            console.log('BREAKPOINT 5, USERS: ' + JSON.stringify(users));
-
-        });
-    }
-    catch(error) { 
-        console.log(error);
-    }
+        socket.on("acceptCall", (data) => {
+            io.to(data.to).emit('callAccepted', data.signal);
+        })
+    });
 
     return router;
 };
