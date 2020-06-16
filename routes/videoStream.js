42d375b3f8547aba55a6021e9a643ac438960d99
marduzca <arduz.miguel@gmail.com>
2020-06-16 15:58:44 +0200

    Small updates

--- routes/videoStream.js
+++ routes/videoStream.js
@@ -9,18 +9,18 @@ module.exports = function (io) {
             users[socket.id] = socket.id;
         }
 
-        socket.emit("yourID", socket.id);
-        io.sockets.emit("allUsers", users);
+        socket.emit('yourID', socket.id);
+        io.sockets.emit('allUsers', users);
 
         socket.on('disconnect', () => {
             delete users[socket.id];
         })
 
-        socket.on("callUser", (data) => {
+        socket.on('callUser', (data) => {
             io.to(data.userToCall).emit('hey', { signal: data.signalData, from: data.from });
         })
 
-        socket.on("acceptCall", (data) => {
+        socket.on('acceptCall', (data) => {
             io.to(data.to).emit('callAccepted', data.signal);
         })
     });
