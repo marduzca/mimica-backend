647782984872cc33a45de349db8a074ce11fdd5f
marduzca <arduz.miguel@gmail.com>
2020-07-15 12:33:13 +0200

    Add unnecessary test code

--- routes/dummy.js
+++ routes/dummy.js
@@ -4,8 +4,6 @@ const dummyRepo = require('../repository/dummy_repository');
 const roomRepo = require('../repository/room_repository');
 
 router.get('/', async function(req, res, next) {
-    await roomRepo.createRoom('an32ms3', 'sfiu23nc', 'Viguelon');
-    await roomRepo.addPlayerToRoom('an32ms3', '2kc9cs', 'Elotro');
     const users = await dummyRepo.getUsers();
     res.status(200).json(users[0]);
 })
