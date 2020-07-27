a7099ccfc4582f6a77efabd0968d08949cc38f3e
marduzca <arduz.miguel@gmail.com>
2020-07-27 18:52:46 +0200

    Refactor modules and extract functions

--- repository/room_repository.js
+++ repository/room_repository.js
@@ -2,102 +2,117 @@ const { Client } = require('pg');
 const client = new Client();
 client.connect();
 
-module.exports = {
-    createRoom: async (roomID, hostID, hostName) => {
-        try {
-            await client.query(
-                `CREATE TABLE ROOM_${roomID} ( ` +
-                'id VARCHAR(20) PRIMARY KEY, ' +
-                'host BOOLEAN NOT NULL, ' +
-                'name VARCHAR(20) NOT NULL, ' +
-                'points INT DEFAULT 0, ' +
-                'ranking INT DEFAULT 1 );');
+async function createRoom(roomID, hostID, hostName) {
+    try {
+        await client.query(
+            `CREATE TABLE ROOM_${roomID} ( ` +
+            'id VARCHAR(20) PRIMARY KEY, ' +
+            'host BOOLEAN NOT NULL, ' +
+            'name VARCHAR(20) NOT NULL, ' +
+            'points INT DEFAULT 0, ' +
+            'ranking INT DEFAULT 1 );'
+        );
+    }
+    catch (err) {
+        throw err;
+    }
+}
 
-            await client.query(
-                `INSERT INTO ROOM_${roomID} (host, id, name) ` +
-                'VALUES(TRUE, $1, $2);', [hostID, hostName]);
-        }
-        catch (err) {
-            throw err;
-        }
-    },
+async function addPlayerToRoom(roomID, isHost, playerID, playerName) {
+    try {
+        await client.query(
+            `INSERT INTO ROOM_${roomID} (host, id, name) ` +
+            'VALUES($1, $2, $3);', [isHost, playerID, playerName]
+        );
+    }
+    catch (err) {
+        throw err;
+    }
+}
 
-    addPlayerToRoom: async (roomID, playerID, playerName) => {
-        try {
-            await client.query(
-                `INSERT INTO ROOM_${roomID} (host, id, name) ` +
-                'VALUES(FALSE, $1, $2);', [playerID, playerName]);
-        }
-        catch (err) {
-            throw err;
-        }
-    },
-    
-    getPlayersInRoom: async (roomID) => {
-        try {
-            const players = await client.query(`SELECT name, host FROM ROOM_${roomID};`);
+async function getPlayersInRoom(roomID) {
+    try {
+        const players = await client.query(`SELECT name, host FROM ROOM_${roomID};`);
 
-            return players.rows;
-        }
-        catch (err) {
-            throw err;
-        }
-    },
+        return players.rows;
+    }
+    catch (err) {
+        throw err;
+    }
+}
 
+async function removePlayerFromRoom(roomID, playerID) {
+    try {
+        await client.query(
+            `DELETE FROM ROOM_${roomID} ` +
+            'WHERE id = $1;', [playerID]
+            );
+    }
+    catch (err) {
+        throw err;
+    }
+}
 
-    removePlayerFromRoom: async (roomID, playerID) => {
-        try {
-            await client.query(
-                `DELETE FROM ROOM_${roomID} ` +
-                'WHERE id = $1;', [playerID]);
-        }
-        catch (err) {
-            throw err;
-        }
-    },
+async function closeRoom(roomID) {
+    try {
+        await client.query(`DROP TABLE ROOM_${roomID};`);
+    }
+    catch (err) {
+        throw err;
+    }
+}
 
-    closeRoom: async (roomID) => {
-        try {
-            await client.query(`DROP TABLE ROOM_${roomID};`);
-        }
-        catch (err) {
-            throw err;
-        }
-    },
-
-    isRoomEmpty: async (roomID) => {
-        try {
-            const isTableEmpty = await client.query(
-                'SELECT CASE ' +
-                `WHEN EXISTS (SELECT * FROM ROOM_${roomID} LIMIT 1) THEN FALSE ` +
-                'ELSE TRUE ' +
-                'END');
-
-            return isTableEmpty.rows[0].case;
-        }
-        catch (err) {
-            throw err;
-        }
-    },
-
-    assignNewHostIfNecessary: async (roomID, playerID) => {
-        try {
-            const isHost = await client.query(
-                `SELECT host FROM ROOM_${roomID} ` +
-                'WHERE id = $1;', [playerID]
+async function isRoomEmpty(roomID) {
+    try {
+        const isTableEmpty = await client.query(
+            'SELECT CASE ' +
+            `WHEN EXISTS (SELECT * FROM ROOM_${roomID} LIMIT 1) THEN FALSE ` +
+            'ELSE TRUE ' +
+            'END'
             );
 
-            if (isHost.rows[0].host) {
-                await client.query(
-                    `UPDATE ROOM_${roomID} ` +
-                    'SET host = TRUE ' +
-                    'WHERE id = ' +
-                    `(SELECT id FROM ROOM_${roomID} WHERE id != $1 LIMIT 1 );`, [playerID]
-                );
-            }
-        }
-        catch (err) {
-            throw err;
-        }
+        return isTableEmpty.rows[0].case;
     }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function isRoomHost(roomID, playerID) {
+    try {
+        const isHost = await client.query(
+            `SELECT host FROM ROOM_${roomID} ` +
+            'WHERE id = $1;', [playerID]
+        );
+
+        return isHost.rows[0].host;
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function reassignRoomHost(roomID, playerID) {
+    try {
+        await client.query(
+            `UPDATE ROOM_${roomID} ` +
+            'SET host = TRUE ' +
+            'WHERE id = ' +
+            `(SELECT id FROM ROOM_${roomID} WHERE id != $1 LIMIT 1 );`, [playerID]
+        );
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    createRoom: createRoom,
+    addPlayerToRoom: addPlayerToRoom,
+    getPlayersInRoom: getPlayersInRoom,
+    removePlayerFromRoom: removePlayerFromRoom,
+    deleteRoomData: closeRoom,
+    isRoomEmpty: isRoomEmpty,
+    isRoomHost: isRoomHost,
+    reassignRoomHost: reassignRoomHost
 }
--- service/room/room_manager.js
+++ service/room/room_manager.js
@@ -2,33 +2,46 @@ const roomRepo = require('../../repository/room_repository');
 
 const activeRooms = [];
 
-module.exports = {
-
-    addNewPlayer: async (player) => {
-        if (activeRooms.includes(player.roomID)) {
-            roomRepo.addPlayerToRoom(player.roomID, player.id, player.name);
-        }
-        else {
-            await roomRepo.createRoom(player.roomID, player.id, player.name);
-            activeRooms.push(player.roomID);
-        }
-
-        return await roomRepo.getPlayersInRoom(player.roomID);
-    },
-
-    removePlayer: async (player) => {
-        await roomRepo.assignNewHostIfNecessary(player.roomID, player.id);
-
-        roomRepo.removePlayerFromRoom(player.roomID, player.id);
-
-        if (await roomRepo.isRoomEmpty(player.roomID)) {
-            roomRepo.closeRoom(player.roomID);
-            activeRooms.splice(activeRooms.indexOf(player.roomID), 1);
-        }
-        else {
-            return await roomRepo.getPlayersInRoom(player.roomID);
-        }
-
+async function addNewPlayer(player) {
+    if (activeRooms.includes(player.roomID)) {
+        roomRepo.addPlayerToRoom(player.roomID, false, player.id, player.name);
+    }
+    else {
+        roomRepo.createRoom(player.roomID);
+        roomRepo.addPlayerToRoom(player.roomID, true, player.id, player.name);
+        activeRooms.push(player.roomID);
     }
 
+    return await roomRepo.getPlayersInRoom(player.roomID);
+}
+
+async function removePlayer(player) {
+    changeRoomHostIfNecessary(player.roomID, player.id);
+
+    roomRepo.removePlayerFromRoom(player.roomID, player.id);
+
+    if (await roomRepo.isRoomEmpty(player.roomID)) {
+        closeRoom(player.roomID);
+    }
+    else {
+        return await roomRepo.getPlayersInRoom(player.roomID);
+    }
+}
+
+async function changeRoomHostIfNecessary(roomID, id) {
+    if (await roomRepo.isRoomHost(roomID, id)) {
+        roomRepo.reassignRoomHost(roomID, id);
+    }
+}
+
+function closeRoom(roomID) {
+    roomRepo.deleteRoomData(roomID);
+    activeRooms.splice(activeRooms.indexOf(roomID), 1);
+}
+
+
+module.exports = {
+    activeRooms: activeRooms,
+    addNewPlayer: addNewPlayer,
+    removePlayer: removePlayer
 }
