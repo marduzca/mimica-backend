cb75ea2123d6b3994feafdaee6f40a705c5ba6de
marduzca <arduz.miguel@gmail.com>
2020-08-13 19:39:47 +0200

    New approach for camera broadcast

--- routes/socket_server.js
+++ routes/socket_server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const roomManager = require('../service/room/room_manager');
 
+const users = {};
+
+const socketToRoom = {};
+
 module.exports = function (io) {
     io.on('connection', socket => {
 
@@ -22,17 +26,40 @@ module.exports = function (io) {
         socket.on('startGame', (gameSettings) => {
             io.sockets.in(gameSettings.roomID).emit('startGame', gameSettings);
         });
-        // if (!users[socket.id]) {
-        //     users[socket.id] = socket.id;
-        // }
 
-        // socket.on('callUser', (data) => {
-        //     io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
-        // })
 
-        // socket.on('acceptCall', (data) => {
-        //     io.to(data.to).emit('callAccepted', data.signal);
-        // })
+        socket.on("join room", roomID => {
+            if (users[roomID]) {
+                users[roomID].push(socket.id);
+            } else {
+                users[roomID] = [socket.id];
+            }
+
+            socketToRoom[socket.id] = roomID;
+            const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+            console.log('USERS IN THIS ROOM');
+            console.log(usersInThisRoom);
+            socket.emit("all users", usersInThisRoom);
+        });
+
+        socket.on("sending signal", payload => {
+            io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+        });
+
+        socket.on("returning signal", payload => {
+            io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+        });
+
+        socket.on('disconnect', () => {
+            const roomID = socketToRoom[socket.id];
+            let room = users[roomID];
+            if (room) {
+                room = room.filter(id => id !== socket.id);
+                users[roomID] = room;
+            }
+        });
+
     });
 
     return router;
